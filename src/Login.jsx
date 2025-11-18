3004b962c0204710b67067bf1b52ca930b3f0339
Ahmedelsayed55 <am1096922@gmail.com>
2025-11-18 16:32:56 +0200

    "v-ws"

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,32 +4,33 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import * as Yup from "yup";
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const handleSubmit = (values) => {
-    const dataVrified = true
+    const dataVrified = true;
     if (dataVrified) {
-    toast.success("تم تسجيل الدخول بنجاح")
-      navigate("/")
-      localStorage.setItem("userToken", "loggedIn")
-      localStorage.setItem("userName", values.username)
-    }else{
-      toast.error("فشل تسجيل الدخول")
+      toast.success("تم تسجيل الدخول بنجاح");
+      navigate("/");
+      localStorage.setItem("userToken", "loggedIn");
+      localStorage.setItem("userName", values.username);
+    } else {
+      toast.error("فشل تسجيل الدخول");
     }
   };
   const validateSchema = Yup.object({
-    email: Yup.string().required("يبني انت عبيط  فين البريد ").email("انت شكلك هتتعبني يابني دخل ايميل عدل  😡😡👊"),
+    email: Yup.string()
+      .required("يبني انت عبيط  فين البريد ")
+      .email("انت شكلك هتتعبني يابني دخل ايميل عدل  😡😡👊"),
     password: Yup.string()
       .required("يبني انت عبيط  فين الباسورد  😡👊")
       .min(4, "ياجدع معرفتكش انا كدا بقا طب ماتسيبو فاضي احسن 😡👊")
       .max(20, "ياخي اتقي الله يعني ياكدا ياكدا  😡👊"),
-        username: Yup.string().required("يبني انت عبيط  فين الاسم  😡👊")
-    
+    username: Yup.string().required("يبني انت عبيط  فين الاسم  😡👊"),
   });
   return (
     <div className="flex justify-center items-center h-screen">
       <Formik
         initialValues={{
-            username: "",
+          username: "",
           email: "",
           password: "",
         }}
@@ -45,7 +46,7 @@ const Login = () => {
             placeholder="User Name"
             className="p-2 border rounded w-full input input-info"
           />
-            <ErrorMessage
+          <ErrorMessage
             name="username"
             component="p"
             className="text-red-500 text-sm"
